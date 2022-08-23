5fee9de166d0c7c098a4c9606b9d0fd2269f5b46
Marcelo Urquiza <76020531+marcelourquiza70@users.noreply.github.com>
2022-08-23 16:05:59 -0300

    alterações no arquivo README e no script

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 This repository is intended to keep an example of a linux script for the DIO Linux Bootcamp project - Project 2.
 
 The script will create a Web Server.
+
+Remember to update the script file execution permission with the command: chmod +x create_web_server.sh
--- create_web_server.sh
+++ create_web_server.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
+echo "atualizando servidor!"
+apt-get update
+apt-get upgrade -y
+
 echo "criando web server!"
+echo "instalando o Apache!"
+apt-get install apache2 -y
+
+echo "instalando o unzip"
+apt-get install unzip -y
+
+echo "baixando e descompactando aplicação"
+mkdir /tmp
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+
+echo "movendo aplicação para pasta padrão do apache"
+cd /linux-site-dio-main
+cp -R * /var/www/html
+
+echo "fim da instalação do servidor web!"
